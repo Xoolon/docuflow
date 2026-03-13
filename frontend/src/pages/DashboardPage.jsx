57efc98e52a6d99578f92657f6cad7e9c2ad95a0
Xoolon <151815437+Xoolon@users.noreply.github.com>
2026-03-13 16:50:42 +0300

    ads

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useStore } from '../store/useStore'
 import api from '../utils/api'
-import AdBanner from '../components/AdBanner.jsx'
+import AdBanner from '../components/Adbanner.jsx'
 import {
   ArrowLeftRight, Sparkles, Zap, ArrowRight,
   CheckCircle2, Crown, ShoppingCart,
